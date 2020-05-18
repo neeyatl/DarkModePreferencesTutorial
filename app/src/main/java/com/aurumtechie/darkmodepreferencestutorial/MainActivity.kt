3f3abd707ee04ae81b9c8336f3736ccbf536e6cd
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2020-05-18 09:35:37 +0530

    Added code to make the dark mode settings persist on app closing and restart.

--- app/src/main/java/com/aurumtechie/darkmodepreferencestutorial/MainActivity.kt
+++ app/src/main/java/com/aurumtechie/darkmodepreferencestutorial/MainActivity.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val darkModeValues = resources.getStringArray(R.array.dark_mode_values)
+        // The apps theme is decided depending upon the saved preferences on app startup
+        when (PreferenceManager.getDefaultSharedPreferences(this)
+            .getString(getString(R.string.dark_mode), getString(R.string.dark_mode_def_value))) {
+            darkModeValues[0] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            darkModeValues[1] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            darkModeValues[2] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            darkModeValues[3] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        }
         setContentView(R.layout.activity_main)
     }
 
